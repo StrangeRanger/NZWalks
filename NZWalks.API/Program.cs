ef2418e2cf82b0e23d694ee2b17396cddb72db12
Hunter T. <strangeranger@hthompson.dev>
2025-01-13 13:56:00 -0800

    Inject the DbContext into the services' container

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // Required to use the controllers inside the 'Controllers' folder.
+
+// Inject the DbContext into the services' container.
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+                                                {
+                                                    // Specify the connection string used by the DbContext.
+                                                    // NOTE: Connection string is specified within the
+                                                    // 'appsettings.json' file.
+                                                    options.UseSqlServer(
+                                                        builder.Configuration.GetConnectionString("NZWalksDbContext"));
+                                                });
 
 var app = builder.Build();
 
@@ -16,5 +29,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
+app.MapControllers(); // Required to use the controllers inside the 'Controllers' folder.
 app.Run();

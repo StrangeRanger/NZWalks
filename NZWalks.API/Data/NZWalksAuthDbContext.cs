257f6d35caf5c3ae925ec9aec8412a23ca3bcc9d
Hunter T. <strangeranger@hthompson.dev>
2025-01-24 17:18:17 -0800

    Update formatting via 'dotnet format'

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -9,14 +9,14 @@ public class NZWalksAuthDbContext : IdentityDbContext
 {
     public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
     { }
-    
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
         string readerRoleId = "8e7b1bd2-5779-4020-97ce-0eefcb9aa4c4";
         string writerRoleId = "c9daf664-e9e5-4bfb-a265-df96a4e61861";
-        
+
         List<IdentityRole> roles = new()
         {
             new IdentityRole()
@@ -34,7 +34,7 @@ public class NZWalksAuthDbContext : IdentityDbContext
                 NormalizedName = "WRITER"
             }
         };
-        
+
         builder.Entity<IdentityRole>().HasData(roles);
     }
 }
